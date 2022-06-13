a615c6c6d9c38ae30e1d3097c246b0e65dd1afe7
Pakolight <palyvoda.nota@gmail.com>
2022-06-13 10:25:41 +0200

    create true table

--- main.py
+++ main.py
@@ -3,16 +3,35 @@ from loguru import logger
 from config import URI
 
 
-try:
-    connection = psycopg2.connect(URI, sslmode="require")
-    with connection.cursor() as cur:
-        cur.execute("CREATE TABLE timelist(publisher_id int PRIMARU KEY")
-        logger.error("ERROR DATABASE")
+connection = psycopg2.connect(URI, sslmode="require")
+connection.autocommit = True
 
-except:
-    logger.info("Somthing wrong")
-finally:
-    pass
+with connection.cursor() as cur:
+
+
+'''
+    #создает табличку
+    cur.execute('CREATE TABLE timelist(id_name serial NOT NULL, '
+                'date_st date NOT NULL DEFAULT CURRENT_DATE, '
+                'project text NOT NULL DEFAULT CURRENT_DATE, '
+                'tasks text, '
+                'desc_ex text, '
+                'ex numeric DEFAULT 0, '
+                'time_start time NOT NULL, '
+                'time_end time, '
+                'costs numeric NOT NULL DEFAULT 12, '
+				'arg numeric NOT NUL DEFAULT 3600, '
+                'PRIMARY KEY (id_name));')
+                
+    #заносит данные в базу
+    #cur.execute("""INSERT INTO timelist(time_start, time_end, ex) VALUES('9:30', '18:00', '10')""")
+
+    
+'''
+
+
+
+
 
 
 
