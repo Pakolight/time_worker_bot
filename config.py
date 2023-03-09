53b6e694043bae5522e0b6bd9b9a7eed5eba1e68
Pakolight <106472383+Pakolight@users.noreply.github.com>
2023-03-09 07:42:07 +0100

    Update config.py
    Change URI

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-URI = "postgres://uqlpjsdbzyxsjq:d4bb192a90c1f1508e9322fe6c2bcaf57fde4729e82b05e2bbd12d801a377c3c@ec2-34-247-172-149.eu-west-1.compute.amazonaws.com:5432/d5pk53koo9cn8e"
+URI = "postgres://kcatltgakxkzly:d4a07d2d45962a0f9efce2fa2af40f476f69235ca0277865086d0bdfd3ba5177@ec2-63-32-152-203.eu-west-1.compute.amazonaws.com:5432/d3i7ss0apg3tte"
