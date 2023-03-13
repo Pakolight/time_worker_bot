cd9f1b46fe3b87956ab5d5fb2a125fd9ea53c0b5
Pakolight <106472383+Pakolight@users.noreply.github.com>
2023-03-13 08:04:59 +0100

    Update config.py
    Uri

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-URI = "postgres://kiufbbyxarlskv:837e432f4b5268735aaf3e11fee549460a4adf9833c49b2694aeb49efe926514@ec2-34-252-35-249.eu-west-1.compute.amazonaws.com:5432/ded23edlecs6p6"
+URI = "postgres://kcatltgakxkzly:d4a07d2d45962a0f9efce2fa2af40f476f69235ca0277865086d0bdfd3ba5177@ec2-63-32-152-203.eu-west-1.compute.amazonaws.com:5432/d3i7ss0apg3tte"
